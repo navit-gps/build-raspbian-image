3018bd01b495fa997fa92138216b9aa40abf9547
Pierre GRANDIN <grandinp@altern.org>
2015-04-03 15:53:55 +0000

    Disabled circleci archiving

--- raspbian/build_raspbian_sd_card.sh
+++ raspbian/build_raspbian_sd_card.sh
@@ -267,7 +267,6 @@ echo "finishing ${image}"
 if [ "${image}" != "" ]; then
   kpartx -d ${image}
   echo "created image ${image}"
-  mv ${buildenv}/images/raspbian_basic_${deb_release}_${today}.img $CIRCLE_ARTIFACTS/
 fi
 
 echo "done."
